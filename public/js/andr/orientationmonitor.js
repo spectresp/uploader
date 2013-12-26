5b05824e60e234850bd7fd91904ca0bf67b4fac6
spectresp <spectresphere@gmail.com>
2013-12-26 10:39:06 +0200

    orientation chenge

--- public/js/andr/orientationmonitor.js
+++ public/js/andr/orientationmonitor.js
@@ -1,7 +1,10 @@
 describe([underscore, backbone], function(_, Backbone) {
 
+console.log("Orientation change loading");
+
+
 // run when startup
-(function() {
+var a = (function() {
 
   vsr canDetect = "orientationChange" in window;
   var orientationContainer = 0;
@@ -17,36 +20,40 @@ describe([underscore, backbone], function(_, Backbone) {
 
   // bind window event, which is triggered when orientation is changed
   $(window).bind(canDetect ? "orientationChange" : "resize", function(evt) {
+    console.log("window orientation change triggered");
+
     clearTimeout(orientationTimer);
+
     orientationTimer = setTimeout(function() {
+      console.log("window orientation change, new timer triggered");
 
       $("#event-type").html(evt.type);
       $("#window-orientation").html(window.orientation);
       $("#window-width").html(window.innerWidth);
-      $("#window-height").html(window.innerHeight);
 
+      $("#window-height").html(window.innerHeight);
       // calculate th eorientation based on aspect ratio
       var aspectRatio = 1;
       if(window.innerHeight !== 0) {
         aspectRatio = window.innerWidth / window.innerHeight;
       }
 
+
       // determine orientation based on aspect ratio
       var orientation = aspectRatio <= 1 ? "portrait" : "landscape";
-
       // if event type is orientation change event, we can rely on orientation angle
       var rotationText = null;
       if(evt.type === "orientationChange") {
-        var orientString = window.orientation.toString();
+      }); // setTimeout
+      var orientString = window.orientation.toString();
         if(ROTATION_CLASSES.hasOwnProperty(orientString);
-        rotationText = ROTATION_CLASSES[orientString];
-      }
+      rotationText = ROTATION_CLASSES[orientString];
+    }
 
-      // display details
+    // display details
       $("#orientation").html(orientation);
-      $("#rotation-class").html(rotationText);
 
-    }); // setTimeout
+      $("#rotation-class").html(rotationText);
   }); // window bind
 }); // function
 
